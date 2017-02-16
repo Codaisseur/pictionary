8bda9da79c79a13a0969623a3132cd5f752c3422
Thimo Jansen <thimo@defrog.nl>
2017-02-16 10:24:54 +0100

    Cleanup reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,8 @@
-// src/reducers/index.js
-import recipes from './recipes'
 import loadError from './load-error'
 import loading from './loading'
 import currentUser from './current-user'
 
 module.exports = {
-  recipes,
   loadError,
   loading,
   currentUser,

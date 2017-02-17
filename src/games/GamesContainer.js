e8ed0f2126355d9038d1284217a8bb59c423754c
Thimo Jansen <thimo@defrog.nl>
2017-02-17 15:08:25 +0100

    Improved layout

--- src/games/GamesContainer.js
+++ src/games/GamesContainer.js
@@ -5,9 +5,10 @@ import addGame from '../actions/games/add-game'
 import subscribeToGamesService from '../actions/games/subscribe'
 import Title from '../components/Title'
 import GameItem from './GameItem'
+import Paper from 'material-ui/Paper'
 import RaisedButton from 'material-ui/RaisedButton'
 import './GamesContainer.sass'
-import { buttonStyle } from '../styles/app.js'
+import { buttonStyle, dialogStyle } from '../styles/app.js'
 
 export class GamesContainer extends PureComponent {
   static propTypes = {
@@ -33,21 +34,19 @@ export class GamesContainer extends PureComponent {
 
   render() {
     return(
-      <div className="games wrapper">
-        <header>
-          <Title content="All Games" />
-        </header>
-
+      <Paper style={ dialogStyle }>
         <RaisedButton
           style={ buttonStyle }
           onClick={ this.addGameClick.bind(this) }
           label="Add game"
           primary={true} />
 
+        <Title content="All Games" />
+
         <main className="container">
           { this.props.games.map(this.renderGame.bind(this)) }
         </main>
-      </div>
+    </Paper>
     )
   }
 }

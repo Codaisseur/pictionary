7b8cacb863f91fbe2d7684e8c02b5c375ac4cd82
Thimo Jansen <thimo@defrog.nl>
2017-02-17 15:00:40 +0100

    Adds "add game" action and button

--- src/games/GamesContainer.js
+++ src/games/GamesContainer.js
@@ -1,16 +1,21 @@
 import React, { PureComponent, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import fetchGames from '../actions/games/fetch'
+import addGame from '../actions/games/add-game'
 import subscribeToGamesService from '../actions/games/subscribe'
 import Title from '../components/Title'
 import GameItem from './GameItem'
+import RaisedButton from 'material-ui/RaisedButton'
 import './GamesContainer.sass'
+import { buttonStyle } from '../styles/app.js'
 
 export class GamesContainer extends PureComponent {
   static propTypes = {
     games: PropTypes.array.isRequired,
     fetchGames: PropTypes.func.isRequired,
     subscribeToGamesService: PropTypes.func.isRequired,
+    addGame: PropTypes.func.isRequired,
+    currentUser: PropTypes.object.isRequired,
   }
 
   componentDidMount() {
@@ -22,12 +27,22 @@ export class GamesContainer extends PureComponent {
     return <GameItem key={ index } { ...game } />
   }
 
+  addGameClick() {
+    this.props.addGame(this.props.currentUser)
+  }
+
   render() {
     return(
       <div className="games wrapper">
         <header>
           <Title content="All Games" />
         </header>
+
+        <RaisedButton
+          style={ buttonStyle }
+          onClick={ this.addGameClick.bind(this) }
+          label="Add game"
+          primary={true} />
 
         <main className="container">
           { this.props.games.map(this.renderGame.bind(this)) }
@@ -37,9 +52,10 @@ export class GamesContainer extends PureComponent {
   }
 }
 
-const mapStateToProps = ({ games }) => ({ games })
+const mapStateToProps = ({ games, currentUser }) => ({ games, currentUser })
 
 export default connect(mapStateToProps, {
   fetchGames,
-  subscribeToGamesService
+  subscribeToGamesService,
+  addGame,
 })(GamesContainer)
